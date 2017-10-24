9cff0ddf6d5a18b4e0158c9aa8e6b12e50f7cf7d
Meteor-Furet <justine.marlow@etu.utc.fr>
2017-10-24 21:01:14 +0200

    Ajout menu

--- main.c
+++ main.c
@@ -4,24 +4,107 @@
 
 int main()
 {
-    T_Liste* liste1 =creerListe();
-    insererElement(liste1, "test");
-    insererElement(liste1, "mamamia");
-    afficher_Liste(liste1);
-    afficher_Element(rechercherElement(liste1,"test"));
+    unsigned int i=0; //nombre de listes créées
+    unsigned int k,k1,k2;
+    T_Liste* tab[5];
+    char valeur[20];
+    int res;
+    T_Element* a_trouver=NULL;
 
-    T_Liste* liste2 =creerListe();
-    insererElement(liste2, "abracadabra");
-    insererElement(liste2, "zorro");
-    insererElement(liste2, "beta");
-    afficher_Liste(liste2);
-    int res=supprimerElement(liste2,"beta");
-    afficher_Liste(liste2);
+    int menu;
+    do
+    {
+    printf("\n Menu \n");
+    printf("1. Créer une liste \n");
+    printf("2. Ajouter un élément dans une liste \n");
+    printf("3. Supprimer un élément d'une liste \n");
+    printf("4. Rechercher un élément dans une liste \n");
+    printf("5. Afficher les éléments d'une liste \n");
+    printf("6. Supprimer une liste \n");
+    printf("7. Fusionner deux listes \n");
+    printf("8. Quitter \n");
+    scanf("%d",&menu);
 
-    T_Liste* liste3 =fusionnerListes(liste1,liste2);
-    afficher_Liste(liste3);
-    supprimerListe(liste3);
-    afficher_Liste(liste3);
-
+    switch (menu) {
+        case 1: //Créer une liste
+            if(i==5) printf("Impossible de créer une nouvelle liste \n");
+            else {
+                tab[i] = creerListe();
+                printf("Vous avez créé une liste numéro %d \n",i+1);
+                i++;
+            }
+            break;
+        case 2: //Ajouter un élément dans une liste
+            printf("Entrez le numéro de la liste \n");
+            scanf("%d",&k);
+            if (k>i+1) printf("Erreur, cette liste n'existe pas \n");
+            else {
+                printf("Entrez la chaîne de caractère à ajouter \n");
+                scanf("%s",&valeur);
+                insererElement(tab[k-1],valeur);
+            }
+            break;
+        case 3: //Supprimer un élément d'une liste
+            printf("Entrez le numéro de la liste \n");
+            scanf("%d",&k);
+            if (k>i+1) printf("Erreur, cette liste n'existe pas \n");
+            else {
+                printf("Entrez la chaîne de caractère à supprimer \n");
+                scanf("%s",&valeur);
+                res=supprimerElement(tab[k-1],valeur);
+                if (res==0) printf("Element supprimé \n");
+                else printf("Problème dans la suppression \n");
+            }
+            break;
+        case 4: //Rechercher un élément dans une liste
+            printf("Entrez le numéro de la liste \n");
+            scanf("%d",&k);
+            if (k>i+1) printf("Erreur, cette liste n'existe pas \n");
+            else {
+                printf("Entrez la chaîne de caractère à rechercher \n");
+                scanf("%s",&valeur);
+                a_trouver=rechercherElement(tab[k-1],valeur);
+                if (a_trouver!=NULL) printf("Element trouvé \n");
+                else printf("Problème dans la recherche \n");
+                a_trouver=NULL;
+            }
+            break;
+        case 5: //Afficher les éléments d'une liste
+            printf("Entrez le numéro de la liste à afficher \n");
+            scanf("%d",&k);
+            if (k>i+1) printf("Erreur, cette liste n'existe pas \n");
+            else afficher_Liste(tab[k-1]);
+            break;
+        case 6: //Supprimer une liste
+            printf("Entrez le numéro de la liste à supprimer \n");
+            scanf("%d",&k);
+            if (k>i+1) printf("Erreur, cette liste n'existe pas \n");
+            else supprimerListe(tab[k-1]);
+            break;
+        case 7:
+            if(i==5) printf("Impossible de créer une nouvelle liste pour la fusion \n");
+            else {
+            printf("Entrez le numéro de la 1ere liste à fusionner \n");
+            scanf("%d",&k1);
+            if (k1>i+1) printf("Erreur, cette liste n'existe pas \n");
+            else {
+                    printf("Entrez le numéro de la 2eme liste à fusionner \n");
+                    scanf("%d",&k2);
+                    if (k2>i+1) printf("Erreur, cette liste n'existe pas \n");
+                    else {
+                            tab[i]=fusionnerListes(tab[k1-1],tab[k2-1]);
+                            printf("La liste %d est la fusion de la liste %d et de la liste %d \n",i+1,k1,k2);
+                            i++;
+                        }
+                }
+            }
+            break;
+        case 8: //Quitter
+            printf("A bientot");
+            break;
+        default : printf("Erreur, veuillez recommencer \n");
+    }
+    }
+    while(menu!=8);
     return 0;
 }
