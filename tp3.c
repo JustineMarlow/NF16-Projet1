ecf8d38a918cec594907e490e00e6a4e99e8b3b9
IlyesH <32774659+IlyesH@users.noreply.github.com>
2017-10-25 10:53:06 +0200

    Correction bug suppression liste nulle

--- tp3.c
+++ tp3.c
@@ -18,15 +18,13 @@ T_Liste *creerListe () {
 
 void afficher_Liste(T_Liste * liste) {
     unsigned int i;
-    T_Element* curseur=malloc(sizeof(T_Element));
-    curseur=liste->tete;
+    T_Element* curseur=liste->tete;
     printf("taille : %d \n", liste->taille);
     for (i=0; i<liste->taille; i++){
         afficher_Element(curseur);
         curseur=curseur->suivant;
     }
     printf("\n");
-    free(curseur);
 }
 
 void afficher_Element(T_Element * element) {
@@ -43,11 +41,11 @@ int insererElement(T_Liste *list, char *val){
         list->taille++;
         return 0;
     }
-    //sinon, recherche de l'endroit où il faut insérer
+    //sinon, recherche de l'endroit oË˜ il faut insÃˆrer
     T_Element *curseur = list->tete;
     while(curseur->suivant != NULL && strcmp(curseur->valeur,val)<0) curseur = curseur->suivant;
     if(strcmp(curseur->valeur,val) == 0) {//free(curseur);
-    return -1; } //échec car existe déjà
+    return -1; } //Ãˆchec car existe dÃˆjâ€¡
 
     //insertion en tete
     if(curseur == list->tete && strcmp(curseur->valeur,val)>0){
@@ -74,19 +72,26 @@ int insererElement(T_Liste *list, char *val){
     return 0;
 }
 
-T_Element *rechercherElement(T_Liste *list, char *val){
+T_Element *rechercherElement(T_Liste* list, char *val){
 
     if(list->taille == 0) return NULL;
-    T_Element *curseur = malloc(sizeof(T_Element));
-    curseur = list->tete;
+    T_Element *curseur = list->tete;
     while(curseur != NULL && strcasecmp(curseur->valeur,val)<0) curseur = curseur->suivant;
     return curseur;
 }
 
 int supprimerElement(T_Liste* list, char* val){
 
+    if (list->taille==0) return -1; //liste vide
     T_Element* curseur = rechercherElement(list, val);
-    if (curseur==NULL) return -1; //liste vide ou élément pas dans la liste
+    if (curseur==NULL) return -1; //ÃˆlÃˆment pas dans la liste
+    if (curseur==list->tete && curseur==list->queue){
+        list->tete=NULL;
+        list->queue=NULL;
+        free(curseur);
+        list->taille--;
+        return 0;
+    }
     if (curseur==list->tete){
         curseur->suivant->precedent=NULL;
         list->tete=curseur->suivant;
@@ -96,7 +101,7 @@ int supprimerElement(T_Liste* list, char* val){
     }
     if (curseur==list->queue){
         curseur->precedent->suivant=NULL;
-        list->queue=curseur->suivant;
+        list->queue=curseur->precedent;
         free(curseur);
         list->taille--;
         return 0;
@@ -108,3 +113,54 @@ int supprimerElement(T_Liste* list, char* val){
     list->taille--;
     return 0;
 };
+
+
+int supprimerListe(T_Liste* list){
+    T_Element *curseur = list->tete;
+    T_Element *tmp;
+    while(curseur != NULL && curseur->suivant != NULL){
+        tmp = curseur->suivant;
+        free(curseur);
+        curseur = tmp;
+    }
+    free(list);
+    //T_Liste* nouvelle = creerListe ();
+    //int res=insererElement(nouvelle, "haha");
+    return 0;
+};
+
+void ajout_queue(T_Liste* list, char* val){
+    T_Element* nouveau = creerElement(val);
+    if (list->taille==0) {
+        list->tete=nouveau;
+        list->queue=nouveau;
+        list->taille++;}
+    else {
+        list->queue->suivant=nouveau;
+        nouveau->precedent=list->queue;
+        list->taille++;
+        list->queue=nouveau;
+    };
+};
+
+T_Liste *fusionnerListes(T_Liste *list1, T_Liste *list2){
+    T_Liste* fusion=creerListe();
+    T_Element* curseur1=list1->tete;
+    T_Element* curseur2=list2->tete;
+
+    while(curseur1!=NULL || curseur2!=NULL){
+        if (curseur1==NULL || (curseur2!=NULL && strcmp(curseur1->valeur,curseur2->valeur)>=0)){
+            ajout_queue(fusion,curseur2->valeur);
+            curseur2=curseur2->suivant;
+        }
+        else {
+                if (curseur2==NULL || (curseur1!=NULL && strcmp(curseur1->valeur,curseur2->valeur)<0)){
+                    ajout_queue(fusion,curseur1->valeur);
+                    curseur1=curseur1->suivant;
+                }
+        }
+    }
+    return fusion;
+};
+
+
